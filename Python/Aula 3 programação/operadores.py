ffa056030513d82ec5b45f3122737e44dfbc5e17
EdnerPauloS <edner.paulo.dias@gmail.com>
2025-02-27 21:41:37 -0300

    Add files via upload

---
+++ Python/Aula 3 programação/operadores.py
@@ -0,0 +1,8 @@
+numero = float(input('Digite um numero positivo  e inteiro : '))
+numero1 = float(input('Digite outro numero positivo e inteiro : '))
+print(' A soma do ',numero,'+',numero1,' é igual a ',numero + numero1)
+print(' A subtração do ',numero,'-',numero1,' é igual a ',numero - numero1)
+print(' A multiplicação do ',numero,'x',numero1,' é igual a ',numero * numero1)
+print(' A divisão do ',numero,'/',numero1,' é igual a ', round(numero / numero1,2))
+# o round serve para definir o numero de casas decimais
+#round( variavel / variavel , 2) a virgula 2 e a quantidade de casas decimais
---
+++ Python/Aula 3 programação/test.py
@@ -0,0 +1,25 @@
+# comentario com a regitegue para fazer
+# para carregar o codigo ---> F5 ou Run Module
+# um nome seguido de ( ) e uma funçao
+
+# serve  para verificar qual o  tipo de dado que estamos declarando 
+# type()
+
+# o print serve para mostrar algo no ambiente de teste . 
+#uma função de ação/ de saida na tela
+# dados do tipo texto
+print(type('Boa noite estou aprendendo Python denovo'))
+# dados do tipo interio
+print(type(10))
+# dados do tipo float ou Real
+print(type(10.5))
+# dados do tipo Boleano
+print(type(True))
+print(10+20)
+n = 10
+print(n)
+# o input server para inserir dados 
+nome = input('Digite um Nome: ')
+idade = input('Digite sua idade: ')
+print('Seu nome é ',nome,' e tem',idade, 'anos.' )
+
