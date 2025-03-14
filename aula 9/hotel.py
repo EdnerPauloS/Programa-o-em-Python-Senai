cbb0025ecf3cb632cc9884726adca923a58ef578
EdnerPauloS <edner.paulo.dias@gmail.com>
2025-03-14 19:21:51 -0300

    Add files via upload

--- aula 9/hotel.py
+++ aula 9/hotel.py
@@ -11,16 +11,87 @@ quartos = {
 if reserva == 's' :
     nome = input('Por favor, digite seu nome: ')
     idade = int(input("Qual a sua idade: "))
-if idade < 18 :
-    print ('Voce não pode reservar um quarto, chame um responsavel.')
+if idade < 18:
+        print('Você não pode reservar um quarto, chame um responsável.')
 else:
-    print('Foi cadastrado com Sucesso')
-    print('Escolha  um de nossos quartos:',quartos)
-    quarto = input('')
-    if quar
-    dias = int(input('Quandos dias será sua reserva: '))
-    total = quartos * dias
-    print(nome,' escolha  o quarto',quartos,' e vai se ospedar por ',dias,' pagando o valor total de ',total)
+        print('Cadastro realizado com sucesso!')
+        print('Escolha um de nossos quartos: ')
+        print('1 - Simples: R$100')
+        print('2 - Duplo: R$150')
+        print('3 - Luxo: R$250')
 
+quarto = int(input('Digite o número do quarto que deseja reservar: '))
+        
+if quarto in quartos:
+    nome_quarto, preco = quartos[quarto]
+    dias = int(input('Quantos dias será sua reserva? '))
+    total = preco * dias
+    print(f'{nome}, você escolheu o quarto {nome_quarto} e ficará hospedado por {dias} dias, pagando o valor total de R${total}.')
+else:
+    print('Opção de quarto inválida, tente novamente.')
+print()
+print()
+print("Proxima reserva")
+print('Olá seja Bem Vindo Ao Aconchego Hotel.')
+reserva = input('Gostaria de fazer uma reserva. s/n ')
+quartos = {
+     1 :['Simples', 100],
+     2 :['Duplo'  , 150],
+     3 :['Luxo'   , 250],
+}
+if reserva == 's' :
+    nome = input('Por favor, digite seu nome: ')
+    idade = int(input("Qual a sua idade: "))
+if idade < 18:
+        print('Você não pode reservar um quarto, chame um responsável.')
+else:
+        print('Cadastro realizado com sucesso!')
+        print('Escolha um de nossos quartos: ')
+        print('1 - Simples: R$100')
+        print('2 - Duplo: R$150')
+        print('3 - Luxo: R$250')
+        
+quarto = int(input('Digite o número do quarto que deseja reservar: '))
+        
+if quarto in quartos:
+    nome_quarto, preco = quartos[quarto]
+    dias = int(input('Quantos dias será sua reserva? '))
+    total = preco * dias
+    print(f'{nome}, você escolheu o quarto {nome_quarto} e ficará hospedado por {dias} dias, pagando o valor total de R${total}.')
+else:
+    print('Opção de quarto inválida, tente novamente.')
+
+
+print()
+print()
+print("Proxima reserva")
+print('Olá seja Bem Vindo Ao Aconchego Hotel.')
+reserva = input('Gostaria de fazer uma reserva. s/n ')
+quartos = {
+     1 :['Simples', 100],
+     2 :['Duplo'  , 150],
+     3 :['Luxo'   , 250],
+}
+if reserva == 's' :
+    nome = input('Por favor, digite seu nome: ')
+    idade = int(input("Qual a sua idade: "))
+if idade < 18:
+        print('Você não pode reservar um quarto, chame um responsável.')
+else:
+        print('Cadastro realizado com sucesso!')
+        print('Escolha um de nossos quartos: ')
+        print('1 - Simples: R$100')
+        print('2 - Duplo: R$150')
+        print('3 - Luxo: R$250')
+        
+quarto = int(input('Digite o número do quarto que deseja reservar: '))
+        
+if quarto in quartos:
+    nome_quarto, preco = quartos[quarto]
+    dias = int(input('Quantos dias será sua reserva? '))
+    total = preco * dias
+    print(f'{nome}, você escolheu o quarto {nome_quarto} e ficará hospedado por {dias} dias, pagando o valor total de R${total}.')
+else:
+    print('Opção de quarto inválida, tente novamente.')
 
 
