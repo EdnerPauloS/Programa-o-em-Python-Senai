ef12d87b4c0cfcd7d0ab7c4550c4c2cb3d4e0f89
EdnerPauloS <edner.paulo.dias@gmail.com>
2025-03-14 20:12:39 -0300

    Add files via upload

--- Exercicios/Ecommerce.py
+++ Exercicios/Ecommerce.py
@@ -2,14 +2,19 @@
 print('Ola seja Bem Vindo.')
 nome = input("Digite o seu nome: ")
 email = input("Qual o seu email: ")
-telefone = input("Qual o seu telefone: ")
+senha = input("Digite uma senha: ")
 print()
+dados ={}
+dados ['nome'] = nome
+dados ['email'] = email
+dados ['senha'] = senha
 print()
 print('Ola, ',nome,'seja Bem Vindo.')
 print('Seu email é: ',email)
-print('E seu telefone é :',telefone)
+print('E seu senha é :',senha)
 print('Esta cadastrado com Sucesso')
 print()
+
 escolha = input('Gostaria de compra nossos produtos, s/n ')
 #comprar um produto 
 produtos = {
@@ -22,6 +27,7 @@ produtos = {
 
 if escolha == 's' :
     print("Este são os nossos Produtos :",produtos)
+    print("Digite o numero do produto que quer comprar: ")
     escolha1 = int(input())
     escolha2 = int(input())
     escolha3 = int(input())
@@ -69,6 +75,40 @@ if escolha == 's' :
 else:
     print('Fim do programa \n VOLTE SEMPRE')
 
+
+# dados = {}
+
+
+# usuario_nome  =  input('Digite seu nome:')
+# usuario_login = input('Digite o login')
+# usuario_senha = input('Digite sua senha')
+
+
+# dados['nome'] = usuario_nome
+# dados['login'] = usuario_login
+# dados['senha'] = usuario_senha
+
+
+# senha_acess = input('Digite a senha')
+# if dados['senha'] == senha_acess:
+#     carrinho = []
+#     meu_total = []
+#     prod = ['IPHONE','DELL', 'MESA DE PC', 'FONE'] 
+#     valores = [10.45,20.25,35.00,50.00]
+#     print('Escolha o produto pelo ID - 0 - 1 - 2 - 3', prod)
+#     escolha_produto1 = int(input('Escolha:'))
+#     escolha_produto2 = int(input('Escolha:'))
+#     carrinho.append(prod[escolha_produto1])
+#     carrinho.append(prod[escolha_produto2])
+#     meu_total.append(valores[escolha_produto1])
+#     meu_total.append(valores[escolha_produto2])
+#     print('Seus produtos', carrinho)
+    
+#     print('>>>>'*10)
+#     soma  =sum( meu_total)
+#     print('Total do pedido R$', soma)
+# else:
+#     print('Acesso negado, senha incorreta')    
 
 
 
